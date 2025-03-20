84aedb7925f3e0ed3c1aa4e644d24f0369ca28a7
mahedimukut <mokot222@gmail.com>
2025-03-20 19:17:20 +0600

    v6

--- src/app/api/jobs/[id]/route.ts
+++ src/app/api/jobs/[id]/route.ts
@@ -60,24 +60,24 @@ export async function DELETE(req: NextRequest, context: { params: { id?: string 
 
 // GET a single job by ID or all jobs if no ID is provided
 
-export async function GET(req: NextRequest, context: { params: { id?: string } }) {
-  try {
-    const id = context.params.id;
+// export async function GET(req: NextRequest, context: { params: { id?: string } }) {
+//   try {
+//     const id = context.params.id;
 
-    if (id) {
-      const job = await prisma.job.findUnique({ where: { id } });
+//     if (id) {
+//       const job = await prisma.job.findUnique({ where: { id } });
 
-      if (!job) {
-        return NextResponse.json({ error: "Job not found" }, { status: 404 });
-      }
+//       if (!job) {
+//         return NextResponse.json({ error: "Job not found" }, { status: 404 });
+//       }
 
-      return NextResponse.json(job, { status: 200 });
-    } else {
-      const jobs = await prisma.job.findMany();
-      return NextResponse.json(jobs, { status: 200 });
-    }
-  } catch (error) {
-    console.error("Error fetching job(s):", error);
-    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
-  }
-}
+//       return NextResponse.json(job, { status: 200 });
+//     } else {
+//       const jobs = await prisma.job.findMany();
+//       return NextResponse.json(jobs, { status: 200 });
+//     }
+//   } catch (error) {
+//     console.error("Error fetching job(s):", error);
+//     return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+//   }
+// }
